3e94f79aef77af8643d33ad03c01ed50c29b39ba
elikaye <elianakaye13@gmail.com>
2026-02-09 16:58:27 -0300

    ✅ Agregado dominio de Vercel a CORS

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ const app = express();
 // 🌐 PERMITIMOS LOCAL Y PRODUCCIÓN (Vercel, etc.)
 const allowedOrigins = [
   'http://localhost:5173',
-  // 'https://tu-dominio.vercel.app',
+  'https://tiendabarby-igaif2836-eliana-kaye.vercel.app',
 ];
 
 // ✅ Configuración segura de CORS
